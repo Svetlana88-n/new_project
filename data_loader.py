db23b58dccf44307ab5e2d89f84c6175c84dbb4a
Svetlana88-n <kalaturskaya.sveta@yandex.ru>
2025-09-29 17:59:53 +0300

    Добавлено приведение столбцов DataFrame в наиболее подходящий тип данных

--- data_loader.py
+++ data_loader.py
@@ -8,3 +8,13 @@ def read_dataset(url):
 url = 'https://drive.google.com/uc?id=1C_iqgpDk2RqQJzV_5J2Nok8DfFMomVbQ'
 df = read_dataset(url)
 
+print("Original Data Types:")
+print(df.dtypes)
+
+newdf = df.convert_dtypes()
+
+print(" \nNew Data Types:")
+print(newdf.dtypes)
+
+df.to_parquet('data.parquet', index=False)
+print("Data save in data.parquet")
