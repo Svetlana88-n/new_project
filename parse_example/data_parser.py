f4cc9d1589e7ed34d4fc2d9d12ce9c3206b79368
Svetlana88-n <kalaturskaya.sveta@yandex.ru>
2025-10-03 20:44:28 +0300

    Parser for Befree

--- parse_example/data_parser.py
+++ parse_example/data_parser.py
@@ -1,5 +1,6 @@
 from LxmlSoup import LxmlSoup
 import requests
+import re
 
 html = requests.get('https://befree.ru/zhenskaya/odezhda').text
 soup = LxmlSoup(html)
@@ -8,8 +9,14 @@ links = soup.find_all('a', class_='sc-4a450f6d-0')
 
 for i, link in enumerate(links):
     url = link.get("href")
-    name = link.text()
-    price = soup.find_all("div", class_="sc-7b424381-0 fzBzDT")[i].text()
+    full_text = link.text()
+    name = full_text
+
+    price_match = re.search(r'(\d+₽)', full_text)
+    if price_match:
+        price = price_match.group(1)
+        name = re.sub(r'\d+₽', '', full_text).strip()
+
     print(i)
     print(f"Url - {url}")
     print(f"Name - {name}")
